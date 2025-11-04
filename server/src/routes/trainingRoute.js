b933a31f07ccdeaa9b90d4443cb78735f283f6f7
a4koshin <mankajr11@gmail.com>
2025-11-04 00:14:41 -0400

    training Model added Joi validation

--- server/src/routes/trainingRoute.js
+++ server/src/routes/trainingRoute.js
@@ -7,15 +7,17 @@ import {
   deleteTraining,
 } from "../controllers/trainingController.js";
 import { protectHR } from "../middlewares/authMiddleware.js";
+import {validate} from "../middlewares/validate.js";
+import { trainingSchema } from "../validation/trainingJoi.js";
 
 const trainingRouter = express.Router();
 
 trainingRouter.get("/", protectHR, getTrainings);
 
 
-trainingRouter.post("/", protectHR, createTraining);
-trainingRouter.get("/:id", protectHR, getTraining);
-trainingRouter.put("/:id", protectHR, updateTraining);
-trainingRouter.delete("/:id", protectHR, deleteTraining);
+trainingRouter.post("/", validate(trainingSchema), createTraining);
+trainingRouter.get("/:id", getTraining);
+trainingRouter.put("/:id", updateTraining);
+trainingRouter.delete("/:id", deleteTraining);
 
 export default trainingRouter;
